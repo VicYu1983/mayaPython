2670caf7ae1a589e609274e419e73cb2b7c0abba
vicyu <finalsteve@gmail.com>
2019-07-17 18:12:11 +0800

    update

--- QuickInstall/JFDestructionSkin/JFDestructionSkin.py
+++ QuickInstall/JFDestructionSkin/JFDestructionSkin.py
@@ -13,86 +13,87 @@ def createLocator( fromName, pos ):
     cmds.select(locaName, add=True)   
     cmds.parentConstraint(mo=False, weight=1)
     return locaName[0]
-    
-# get selected mesh
-objs = cmds.ls(sl=True, o=True)
 
-# collect mesh vertex count
-pushObj = []
-for obj in objs:
-    cmds.select(obj)
-    pushObj.append({'name':obj, 'count':cmds.polyEvaluate( v=True )})
+def execute():
+    # get selected mesh
+    objs = cmds.ls(sl=True, o=True)
 
-# create root bone
-rootLocator = cmds.spaceLocator()
+    # collect mesh vertex count
+    pushObj = []
+    for obj in objs:
+        cmds.select(obj)
+        pushObj.append({'name':obj, 'count':cmds.polyEvaluate( v=True )})
 
-# create bone on every mesh position and constraint
-locators = []
-for obj in objs:
-    pos = cmds.getAttr("%s.translate" % obj)
-    locators.append( createLocator( obj.__str__(), pos[0] ))
+    # create root bone
+    rootLocator = cmds.spaceLocator()
 
-# link all bone to root bone
-cmds.select(clear=True)
-cmds.select(locators)
-cmds.parent( locators, rootLocator )
+    # create bone on every mesh position and constraint
+    locators = []
+    for obj in objs:
+        pos = cmds.getAttr("%s.translate" % obj)
+        locators.append( createLocator( obj.__str__(), pos[0] ))
 
-# bake all bone animation 
-cmds.bakeResults( locators,
-                    simulation=True,
-                    t=(1,120),
-                    sampleBy=1,
-                    disableImplicitControl=True,
-                    preserveOutsideKeys=False,
-                    sparseAnimCurveBake=False,
-                    removeBakedAttributeFromLayer=False,
-                    bakeOnOverrideLayer=False,
-                    minimizeRotation=False,
-                    at=['tx','ty','tz','rx','ry','rz'] )
-                  
-# combine all mesh
-cmds.select(objs)
-cmds.polyUnite(n='result')
+    # link all bone to root bone
+    cmds.select(clear=True)
+    cmds.select(locators)
+    cmds.parent( locators, rootLocator )
 
-# collect all bones and root bone for skin
-skinLocators = [rootLocator[0]]
-skinLocators.extend( locators )
+    # bake all bone animation 
+    cmds.bakeResults( locators,
+                        simulation=True,
+                        t=(1,120),
+                        sampleBy=1,
+                        disableImplicitControl=True,
+                        preserveOutsideKeys=False,
+                        sparseAnimCurveBake=False,
+                        removeBakedAttributeFromLayer=False,
+                        bakeOnOverrideLayer=False,
+                        minimizeRotation=False,
+                        at=['tx','ty','tz','rx','ry','rz'] )
+                    
+    # combine all mesh
+    cmds.select(objs)
+    cmds.polyUnite(n='result')
 
-# select all bones and root bone
-cmds.select(cl=True)
-cmds.select( skinLocators )
+    # collect all bones and root bone for skin
+    skinLocators = [rootLocator[0]]
+    skinLocators.extend( locators )
 
-# add select with result mesh
-cmds.select('result', add=True)
+    # select all bones and root bone
+    cmds.select(cl=True)
+    cmds.select( skinLocators )
 
-# skin
-cmds.skinCluster(tsb=False,tst=True, bm=0, sm=0, nw=1, wd=0, mi=5)
+    # add select with result mesh
+    cmds.select('result', add=True)
 
-# set skin weight 
-vertex_start = 0
-vertex_end = 0
-for loca, mesh in zip(locators, pushObj):
-    vertex_end += mesh['count']
-    cmds.skinPercent( 'skinCluster1', 'result.vtx[{}:{}]'.format(vertex_start, vertex_end), transformValue=[(loca, 1)])
-    vertex_start += mesh['count']
+    # skin
+    cmds.skinCluster(tsb=False,tst=True, bm=0, sm=0, nw=1, wd=0, mi=5)
 
-# delete all constraint
-cmds.select( locators )
-cmds.delete( cn=True )
+    # set skin weight 
+    vertex_start = 0
+    vertex_end = 0
+    for loca, mesh in zip(locators, pushObj):
+        vertex_end += mesh['count']
+        cmds.skinPercent( 'skinCluster1', 'result.vtx[{}:{}]'.format(vertex_start, vertex_end), transformValue=[(loca, 1)])
+        vertex_start += mesh['count']
 
-# disconnect some attribute, if not, can not import to UE4. I don't know why
-cmds.disconnectAttr('result.lockInfluenceWeights', 'skinCluster1.lockWeights[0]') 
-cmds.disconnectAttr('result.objectColorRGB', 'skinCluster1.influenceColor[0]')
-cmds.disconnectAttr('result.worldMatrix[0]', 'skinCluster1.matrix[0]')
+    # delete all constraint
+    cmds.select( locators )
+    cmds.delete( cn=True )
 
-# delete original mesh and useless bindpose
-cmds.select(cl=True)
-cmds.select('bindPose1')
-cmds.delete()
+    # disconnect some attribute, if not, can not import to UE4. I don't know why
+    cmds.disconnectAttr('result.lockInfluenceWeights', 'skinCluster1.lockWeights[0]') 
+    cmds.disconnectAttr('result.objectColorRGB', 'skinCluster1.influenceColor[0]')
+    cmds.disconnectAttr('result.worldMatrix[0]', 'skinCluster1.matrix[0]')
 
-# delete useless node
-cmds.select( 'result' )
-mel.eval('doBakeNonDefHistory( 1, {"prePost" });')
+    # delete original mesh and useless bindpose
+    cmds.select(cl=True)
+    cmds.select('bindPose1')
+    cmds.delete()
+
+    # delete useless node
+    cmds.select( 'result' )
+    mel.eval('doBakeNonDefHistory( 1, {"prePost" });')
 
 
 
